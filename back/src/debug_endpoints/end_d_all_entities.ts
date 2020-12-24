44b7ff1e6c23871408a02905c679bdef6f3e14a2
Dominik Reinert <dominik.reinert.merzig@googlemail.com>
2020-12-24 20:04:45 +0100

    add kreisverband

--- back/src/debug_endpoints/end_d_all_entities.ts
+++ back/src/debug_endpoints/end_d_all_entities.ts
@@ -1,5 +1,4 @@
 import { Response } from "express";
-import { Kreisverband, KreisverbandEntity } from "../entities/kreisverband";
 import { Kurs, KursEntity } from "../entities/kurs";
 import {
   KursTeilnehmer,
@@ -7,13 +6,14 @@ import {
 } from "../entities/kurs_teilnehmer";
 import { Pruefung, PruefungEntity } from "../entities/pruefung";
 import { Verein, VereinEntity } from "../entities/verein";
+import { Kreisverband, KreisverbandDto } from "../table/kreisverband_table";
 import { Landesverband, LandesverbandDto } from "../table/landesverband_table";
 import { Person, PersonDto } from "../table/person_table";
 
 export async function sendAllEntities(res: Response<any>): Promise<any> {
   const allLandesverband: Landesverband[] = await LandesverbandDto.findAll();
   const allPerson: Person[] = await PersonDto.findAll();
-  const allKreisverband: Kreisverband[] = await new KreisverbandEntity().findAll();
+  const allKreisverband: Kreisverband[] = await KreisverbandDto.findAll();
   const allVerein: Verein[] = await new VereinEntity().findAll();
   const allKurs: Kurs[] = await new KursEntity().findAll();
   const allPruefung: Pruefung[] = await new PruefungEntity().findAll();
